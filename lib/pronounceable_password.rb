7b0bcdc5d0d3e276ba830217ba778ffcccc180c3
Jeri <drvonnjerryxlii@gmail.com>
2015-07-16 12:12:20 -0700

    all specs are green.

--- lib/pronounceable_password.rb
+++ lib/pronounceable_password.rb
@@ -1,9 +1,10 @@
 require 'csv'
 
 class PronounceablePassword
+  attr_reader :probabilities
 
   def initialize(probability_corpus)
-    # probability corpus is the file location of the CSV with the 
+    # probability corpus is the file location of the CSV with the
     # pre-calculated letter probability pairs
     @probability_corpus = probability_corpus
   end
@@ -11,19 +12,51 @@ class PronounceablePassword
   def read_probabilities
     # Should consume the provided CSV file into a structure that
     # can be used to identify the most probably next letter
+
+    hash_form = {}
+
+    # !W remove the header row later if time.
+    array_form = CSV.read(@probability_corpus)
+
+    array_form.each { |row|
+      hash_form[row.first] = row.last.to_i
+    }
+
+    @probabilities = hash_form
+
+    return hash_form
   end
 
   def possible_next_letters(letter)
     # Should return an array of possible next letters sorted
     # by likelyhood in a descending order
+
+    # expect(@pronounce.possible_next_letters('z')).to eql [{"za"=>26}, {"zb"=>10}]
+    letter_values = probabilities.select { |key, value| key[0] == letter }
+    ascending_array = letter_values.sort_by { |key, value| value }
+    descending_array = ascending_array.reverse
+    descending_array.map { |array| {array.first => array.last} }
   end
 
   def most_common_next_letter(letter)
     # The most probable next letter
+    best_response = possible_next_letters(letter).first
+    best_response.keys.pop[1]
   end
 
   def common_next_letter(letter, sample_limit = 2)
     # Randomly select a common letter within a range defined by
-    # the sample limit as the lower bounds of a substring 
+    # the sample limit as the lower bounds of a substring
+    responses = possible_next_letters(letter)
+    letters = responses.map{ |hash| hash.keys.pop[1] }
+
+    last = sample_limit - 1
+    return_value = ""
+
+    sample_limit.times do |count|
+      count == last ? return_value = letters.pop : letters.pop
+    end
+
+    return return_value
   end
 end
