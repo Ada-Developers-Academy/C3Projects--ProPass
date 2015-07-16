7acaf5ca32e0dc0cc7fb3ba2384a876bc252ce8a
Loren Wang <wang131.131@gmail.com>
2015-07-16 13:21:43 -0700

    reading the CSV file hopefully...

--- lib/pronounceable_password.rb
+++ lib/pronounceable_password.rb
@@ -1,16 +1,23 @@
 require 'csv'
+require 'pry'
 
 class PronounceablePassword
 
   def initialize(probability_corpus)
-    # probability corpus is the file location of the CSV with the 
+    # probability corpus is the file location of the CSV with the
     # pre-calculated letter probability pairs
     @probability_corpus = probability_corpus
   end
+    # data = Array.new
 
   def read_probabilities
     # Should consume the provided CSV file into a structure that
     # can be used to identify the most probably next letter
+    hash = {}
+    CSV.foreach(@probability_corpus, { headers: true, header_converters: :symbol}) do |row|
+      hash[row[0]] = row[1].to_i
+    end
+    return hash
   end
 
   def possible_next_letters(letter)
@@ -24,6 +31,6 @@ class PronounceablePassword
 
   def common_next_letter(letter, sample_limit = 2)
     # Randomly select a common letter within a range defined by
-    # the sample limit as the lower bounds of a substring 
+    # the sample limit as the lower bounds of a substring
   end
 end
