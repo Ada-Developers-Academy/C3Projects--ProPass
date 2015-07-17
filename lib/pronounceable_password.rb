babc6b5131c7b0707cac823e970e1c3fd10a88a7
Loraine <loraine@lorainekv.com>
2015-07-16 22:43:29 -0700

    Got first spec running. Had fun with variables.

--- lib/pronounceable_password.rb
+++ lib/pronounceable_password.rb
@@ -1,16 +1,28 @@
 require 'csv'
+require 'pry'
 
 class PronounceablePassword
 
   def initialize(probability_corpus)
-    # probability corpus is the file location of the CSV with the 
+    # probability corpus is the file location of the CSV with the
     # pre-calculated letter probability pairs
     @probability_corpus = probability_corpus
   end
 
   def read_probabilities
+
+    mouth_sounds = CSV.read(@probability_corpus, :headers => true)
+
+    hash_fulla_sounds_and_nums = {}
+
+    mouth_sounds.each do |blurp|
+    hash_fulla_sounds_and_nums[blurp[0]] = blurp[1].to_i
+    end
+
+    return hash_fulla_sounds_and_nums
     # Should consume the provided CSV file into a structure that
     # can be used to identify the most probably next letter
+
   end
 
   def possible_next_letters(letter)
@@ -24,6 +36,6 @@ class PronounceablePassword
 
   def common_next_letter(letter, sample_limit = 2)
     # Randomly select a common letter within a range defined by
-    # the sample limit as the lower bounds of a substring 
+    # the sample limit as the lower bounds of a substring
   end
 end
