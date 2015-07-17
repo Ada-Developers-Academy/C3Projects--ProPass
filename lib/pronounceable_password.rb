311a65013d03d6fdd41ce1ebf40d15252ba977ee
Loraine <loraine@lorainekv.com>
2015-07-17 00:00:24 -0700

    Not sure how to convert data into tuples. Other things work. I wonder if we can change the rspecs to fit our preferred format?

--- lib/pronounceable_password.rb
+++ lib/pronounceable_password.rb
@@ -2,11 +2,14 @@ require 'csv'
 require 'pry'
 
 class PronounceablePassword
+  attr_writer :probs
+
 
   def initialize(probability_corpus)
     # probability corpus is the file location of the CSV with the
     # pre-calculated letter probability pairs
     @probability_corpus = probability_corpus
+    @probs = "no_probs"
   end
 
   def read_probabilities
@@ -19,18 +22,27 @@ class PronounceablePassword
     hash_fulla_sounds_and_nums[blurp[0]] = blurp[1].to_i
     end
 
-    return hash_fulla_sounds_and_nums
+    @probs = hash_fulla_sounds_and_nums
     # Should consume the provided CSV file into a structure that
     # can be used to identify the most probably next letter
 
   end
 
   def possible_next_letters(letter)
+
+    matches = @probs.select {|key, value| key.include?(letter)}
+    sorted = matches.sort_by {|sound, probbles| probbles }.reverse
+    return sorted
+
     # Should return an array of possible next letters sorted
-    # by likelyhood in a descending order
+    # by likelihood in a descending order
   end
 
   def most_common_next_letter(letter)
+
+    sorted = possible_next_letters(letter)
+    next_letter = sorted[0][0][1]
+    return
     # The most probable next letter
   end
 
