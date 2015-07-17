252d2bb9937bb6b3b705903b2f625d502e07ef93
Lila Garcia <lilagrc@gmail.com>
2015-07-17 10:12:00 -0700

    Got second test to pass

--- lib/pronounceable_password.rb
+++ lib/pronounceable_password.rb
@@ -3,29 +3,36 @@ require 'pry'
 
 class PronounceablePassword
 
-  def initialize(probability_corpus)
+  def initialize(probability_csv) #renamed for clarity
     # probability corpus is the file location of the CSV with the
     # pre-calculated letter probability pairs
-    @probability_corpus = probability_corpus
+    @probability_csv = probability_csv
   end
 
   def read_probabilities
     # Should consume the provided CSV file into a structure that
     # can be used to identify the most probably next letter
-    hash = {}
+    @hash = {}
 
-    CSV.foreach(@probability_corpus, headers: true) do |row|
-      hash[row[0]] = row[1].to_i
+    CSV.foreach(@probability_csv, headers: true) do |row|
+      @hash[row[0]] = row[1].to_i
     end
-    return hash
+    return @hash
   end
-
-  binding.pry
 
 
   def possible_next_letters(letter)
     # Should return an array of possible next letters sorted
     # by likelyhood in a descending order
+    return_hash = @hash.select do |c|
+      c.start_with?(letter)
+    end
+
+
+    sorted = return_hash.sort_by { |c| c[1] }.reverse!
+
+    sorted_hash = sorted.map { |array| {array[0] => array[1]} }
+    return sorted_hash
   end
 
   def most_common_next_letter(letter)
