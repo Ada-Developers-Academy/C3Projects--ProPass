4813aa6e7c8237c73d3d3e5fcd3e4fb9ad4da008
Loren Wang <wang131.131@gmail.com>
2015-07-17 00:09:13 -0700

    added some comments. common_next_letter still WIP

--- lib/pronounceable_password.rb
+++ lib/pronounceable_password.rb
@@ -15,10 +15,11 @@ class PronounceablePassword
     # can be used to identify the most probably next letter
 
     hash = {}
+
     CSV.foreach(@probability_corpus, { headers: true}) do |row|
       hash[row[0]] = row[1].to_i
-      # row = [ letter_pair, count]
-      # hash[letter_pair] = count
+      # each row in the csv file equals an array with a letter_pair and count
+      # for each letter_pair & count, put it in a hash, and set the key (index 0) => value (index 1)
     end
     return hash
   end
@@ -27,27 +28,28 @@ class PronounceablePassword
     # Should return an array of possible next letters sorted
     # by likelyhood in a descending order
 
-
     all_probabilities_hash = self.read_probabilities
-    # pulling out ones that start with that letter from all_probabilities hash
-    probabilities_array = all_probabilities_hash.select { |letter_pair, count| letter_pair.chars.first == letter }.to_a
-    # letter_pair is the key which is a string. call '.chars.first' to see if the first letter of the string == the input letter and it'll select all the corresponding keys.
-    # returns an array of arrays (each record is a separate array in this huge array)
+    # one giant hash
 
-    probabilities_array.sort_by! { |subarray| -subarray[1] }
-    # negative to get it sorted by descending order
+    probabilities_array = all_probabilities_hash.select { |letter_pair, count| letter_pair.chars.first == letter }.to_a
+    # for each key-value pair in the hash, select the ones that return true if the key's first
+    # character equals the letter input, and insert each into separate arrays within one parent array
+
+    probabilities_array.sort_by! { |sub_array| -sub_array[1] }
+    # sort all the elements of the probabilities_array by each sub_array's 2nd index, 'count,' in descending order
     return probabilities_array
 
   end
 
   def most_common_next_letter(letter)
     # The most probable next letter
+
     sorted_array = self.possible_next_letters(letter)
     sorted_array.first[0][1]
-    # Within the sorted array,
+    # Within the sorted array, find the most common letter_pair, amd return the second letter
   end
 
-  def common_next_letter(letter, sample_limit = 5) # or just sample_limit
+  def common_next_letter(letter, sample_limit = 5) # or just sample_limit??
     # Randomly select a common letter within a range defined by
     # the sample limit as the lower bounds of a substring
     random_choices = self.possible_next_letters(letter)
