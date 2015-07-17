9b602e5e630b33ca5a0403f27caa2da8ed115d6f
Carly Jugler <carjug@gmail.com>
2015-07-17 10:13:33 -0700

    Adds method for possible_next_letters functionality and changes corresponding test to expect an array or arrays, not an array of tuples

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ pronounce.read_probabilities
 pass_length = 10
 starting_letter = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z).sample
 
+binding.pry
+
 # most common probability letter
 most_common_pronouncable_password = [starting_letter]
 while most_common_pronouncable_password.length < pass_length do
--- lib/pronounceable_password.rb
+++ lib/pronounceable_password.rb
@@ -1,9 +1,11 @@
 require 'csv'
+require 'pry'
 
 class PronounceablePassword
+  attr_reader :data
 
   def initialize(probability_corpus)
-    # probability corpus is the file location of the CSV with the 
+    # probability corpus is the file location of the CSV with the
     # pre-calculated letter probability pairs
     @probability_corpus = probability_corpus
   end
@@ -11,11 +13,21 @@ class PronounceablePassword
   def read_probabilities
     # Should consume the provided CSV file into a structure that
     # can be used to identify the most probably next letter
+    @data = {}
+
+    CSV.foreach(@probability_corpus, headers: true, header_converters: :symbol) do |row|
+      @data[row[:letter_pair]] = row[:count].to_i
+    end
+
+    return @data
   end
 
   def possible_next_letters(letter)
     # Should return an array of possible next letters sorted
     # by likelyhood in a descending order
+    all_matches = @data.select { |key| key[0].include?(letter) }
+
+    sorted_letters = all_matches.sort_by { |key, values| -values }
   end
 
   def most_common_next_letter(letter)
@@ -24,6 +36,6 @@ class PronounceablePassword
 
   def common_next_letter(letter, sample_limit = 2)
     # Randomly select a common letter within a range defined by
-    # the sample limit as the lower bounds of a substring 
+    # the sample limit as the lower bounds of a substring
   end
 end
