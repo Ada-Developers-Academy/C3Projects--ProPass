6e9f763d7279305569b31819a71022532e502c01
Anita <asovay@gmail.com>
2015-07-17 20:58:12 -0700

    Added gemset for gemsetting.
    Also completed the app.
    
    Passing all tests. No refactoring done on tests.
    
    Added a tuplize method.

--- lib/pronounceable_password.rb
+++ lib/pronounceable_password.rb
@@ -1,5 +1,4 @@
 require 'csv'
-require 'pry'
 
 class PronounceablePassword
 
@@ -7,31 +6,42 @@ class PronounceablePassword
     # probability corpus is the file location of the CSV with the 
     # pre-calculated letter probability pairs
     @probability_corpus = probability_corpus
+    @probs = {}
   end
 
   def read_probabilities
     # Should consume the provided CSV file into a structure that
     # can be used to identify the most probably next letter
-    @probabilities = []
     CSV.foreach(@probability_corpus, headers: true) do |row|
-     @probabilities << { row[0] => row[1].to_i }
+     @probs[row[0]] = row[1].to_i
+    end
+    return @probs
+  end
+
+  def tupelize(array)
+    array.map do |pair|
+      { pair[0] => pair[1] }
     end
   end
 
   def possible_next_letters(letter)
     # Should return an array of possible next letters sorted
     # by likelihood in a descending order
-    @probabilities.select { |key, value| key.include?(letter) }
-
+    matching_keys = @probs.select { |k,v| k[0] == letter }
+    sorted = matching_keys.sort_by { |k,v| v }.reverse
+    tupelize(sorted)
   end
-  binding.pry
 
   def most_common_next_letter(letter)
     # The most probable next letter
+    da_best = possible_next_letters(letter).first
+    next_letter = da_best.keys.pop[1]
   end
 
   def common_next_letter(letter, sample_limit = 2)
     # Randomly select a common letter within a range defined by
-    # the sample limit as the lower bounds of a substring 
+    # the sample limit as the lower bounds of a substring
+    sample_letter = possible_next_letters(letter)[0...sample_limit]
+    next_letter = sample_letter.sample.keys.pop[1]
   end
 end
