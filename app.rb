f632a5b7d899414e5f25ca8fc1d4496a8fc895a9
Victoria <vikshab2010@hotmail.com>
2015-07-19 11:25:54 -0700

    4 green tests

--- app.rb
+++ app.rb
@@ -1,19 +1,20 @@
 require_relative './lib/pronounceable_password'
 pronounce = PronounceablePassword.new './data/probability.csv'
 pronounce.read_probabilities
-pass_length = 10
-starting_letter = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z).sample
-
+pronounce.possible_next_letters("ho")
+# pass_length = 10
+# starting_letter = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z).sample
+#
 # most common probability letter
-most_common_pronouncable_password = [starting_letter]
-while most_common_pronouncable_password.length < pass_length do
-  most_common_pronouncable_password << pronounce.most_common_next_letter(most_common_pronouncable_password.last)
-end
-puts most_common_pronouncable_password.join
-
-# more random version of above
-common_pronouncable_password = [starting_letter]
-while common_pronouncable_password.length < pass_length do
-  common_pronouncable_password << pronounce.common_next_letter(common_pronouncable_password.last)
-end
-puts common_pronouncable_password.join
+# most_common_pronouncable_password = [starting_letter]
+# while most_common_pronouncable_password.length < pass_length do
+#   most_common_pronouncable_password << pronounce.most_common_next_letter(most_common_pronouncable_password.last)
+# end
+# puts most_common_pronouncable_password.join
+#
+# # more random version of above
+# common_pronouncable_password = [starting_letter]
+# while common_pronouncable_password.length < pass_length do
+#   common_pronouncable_password << pronounce.common_next_letter(common_pronouncable_password.last)
+# end
+# puts common_pronouncable_password.join
