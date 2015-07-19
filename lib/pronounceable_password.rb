92a8bae9f47eb87a78d338d104204008e0e81329
Corinne Pingul <corinnepingul@gmail.com>
2015-07-19 13:10:47 -0700

    Changed the first spec so that the data is stored as an array of hashes.

--- lib/pronounceable_password.rb
+++ lib/pronounceable_password.rb
@@ -10,20 +10,24 @@ class PronounceablePassword
 
   def read_probabilities
     # Should consume the provided CSV file into a structure that
-    # can be used to identify the most probably next letter
-    letter_probabilities = Hash.new
+    # can be used to identify the most probable next letter
+    @letter_probabilities = Array.new
     CSV.foreach(@probability_corpus, :headers => true) do |row|
       letter_combo = row["letter pair"]
       probability = row["count"].to_i
-      letter_probabilities[letter_combo] = probability
+      hash = Hash.new
+      hash[letter_combo] = probability
+      @letter_probabilities.push(hash)
     end
 
-    return letter_probabilities
+    return @letter_probabilities
   end
 
   def possible_next_letters(letter)
     # Should return an array of possible next letters sorted
     # by likelyhood in a descending order
+
+
   end
 
   def most_common_next_letter(letter)
