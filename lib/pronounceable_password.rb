c3743bacbadeae3373e8eee30437606085eae085
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-07-18 19:02:30 -0700

    not-really-WIP.  No progress.

--- lib/pronounceable_password.rb
+++ lib/pronounceable_password.rb
@@ -17,40 +17,42 @@ class PronounceablePassword
     @probabilities.each do |row|
       @dictionary[row[0]] = row[1].to_i
     end
-    print @dictionary
     sort
   end
 
   def sort
     @sorted = @dictionary.sort_by { |key, value| value }.reverse!
+    # sorted is arrays within an array
+    # [["za", 26], ["ya", 25]...["zb", 10]]
+    print @sorted
+    binding.pry
   end
 
   def possible_next_letters(letter)
     # Should return an array of possible next letters sorted
     # by likelyhood in a descending order
 
+
+    @sorted.each do |pair| # ["za", 26]
     possibilites = []
-
-    @sorted.each do |pair|
-      possibilites.push(pair) if pair[0].chars.first == letter
-      binding.pry
+      if pair[0].chars.first == letter
+        possibilites << pair
+      end
     end
-
+    puts "possibilities:"
+    print possibilities
     possibilities
-    binding.pry
-
-
   end
 
   def most_common_next_letter(letter)
     # The most probable next letter
-
-    # @combos.sort_by(values desc).first
+    possible_next_letters(letter).first
   end
 
   def common_next_letter(letter, sample_limit = 2)
     # Randomly select a common letter within a range defined by
     # the sample limit as the lower bounds of a substring
     # @combos.pluck.limit(1) - that are still near the front?????
+    possible_next_letters(letter)
   end
 end
