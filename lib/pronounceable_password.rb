4939ad24b165498381e879cca2b2d73e6cab3842
Corinne Pingul <corinnepingul@gmail.com>
2015-07-19 12:55:55 -0700

    First spec test is green. Read the CSV file and stored the data as a hash. Keys are the letter pairs and values are the probabilities.

--- lib/pronounceable_password.rb
+++ lib/pronounceable_password.rb
@@ -3,7 +3,7 @@ require 'csv'
 class PronounceablePassword
 
   def initialize(probability_corpus)
-    # probability corpus is the file location of the CSV with the 
+    # probability corpus is the file location of the CSV with the
     # pre-calculated letter probability pairs
     @probability_corpus = probability_corpus
   end
@@ -11,6 +11,14 @@ class PronounceablePassword
   def read_probabilities
     # Should consume the provided CSV file into a structure that
     # can be used to identify the most probably next letter
+    letter_probabilities = Hash.new
+    CSV.foreach(@probability_corpus, :headers => true) do |row|
+      letter_combo = row["letter pair"]
+      probability = row["count"].to_i
+      letter_probabilities[letter_combo] = probability
+    end
+
+    return letter_probabilities
   end
 
   def possible_next_letters(letter)
@@ -24,6 +32,6 @@ class PronounceablePassword
 
   def common_next_letter(letter, sample_limit = 2)
     # Randomly select a common letter within a range defined by
-    # the sample limit as the lower bounds of a substring 
+    # the sample limit as the lower bounds of a substring
   end
 end
