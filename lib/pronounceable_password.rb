9bbb7d5d947d89f6c9b76ff07f782f3e1b51ba93
Carly Jugler <carjug@gmail.com>
2015-07-19 18:57:22 -0700

    Adds most common next letter method functionality.

--- lib/pronounceable_password.rb
+++ lib/pronounceable_password.rb
@@ -27,15 +27,21 @@ class PronounceablePassword
     # by likelyhood in a descending order
     all_matches = @data.select { |key| key[0].include?(letter) }
 
-    sorted_letters = all_matches.sort_by { |key, values| -values }
+    @sorted_letters = all_matches.sort_by { |key, values| -values }
+
+    return @sorted_letters
   end
 
   def most_common_next_letter(letter)
-    # The most probable next letter
+    most_common_letter_pair = @sorted_letters.find { |pairs| string = pairs[0]
+       string[0,1] == letter }
+
+    most_common_letter_pair.first[-1]
   end
 
   def common_next_letter(letter, sample_limit = 2)
     # Randomly select a common letter within a range defined by
     # the sample limit as the lower bounds of a substring
+
   end
 end
