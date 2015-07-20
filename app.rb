eca755b6c70e4a8b5a6ac037df78d599446e34db
Carly Jugler <carjug@gmail.com>
2015-07-19 19:33:44 -0700

    Changes while loops in app.rb to until loops :D

--- app.rb
+++ app.rb
@@ -8,14 +8,14 @@ binding.pry
 
 # most common probability letter
 most_common_pronouncable_password = [starting_letter]
-while most_common_pronouncable_password.length < pass_length do
+until most_common_pronouncable_password.length == pass_length do
   most_common_pronouncable_password << pronounce.most_common_next_letter(most_common_pronouncable_password.last)
 end
 puts most_common_pronouncable_password.join
 
 # more random version of above
 common_pronouncable_password = [starting_letter]
-while common_pronouncable_password.length < pass_length do
+until common_pronouncable_password.length == pass_length do
   common_pronouncable_password << pronounce.common_next_letter(common_pronouncable_password.last)
 end
 puts common_pronouncable_password.join
