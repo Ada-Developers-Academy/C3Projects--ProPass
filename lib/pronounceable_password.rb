512b9f2a16b57df784457037530928ddc19ea0a5
jnf <jeremy.flores@gmail.com>
2015-07-20 14:42:50 -0700

    Adds a pronounceable password generation that uses an array of tuples.

--- lib/pronounceable_password.rb
+++ lib/pronounceable_password.rb
@@ -1,6 +1,60 @@
 require 'csv'
 
-class PronounceablePassword
+class PronounceablePasswordWithTupleArrays
+  attr_reader :tuples, :probability_corpus
+
+  def initialize(probability_corpus)
+    @probability_corpus = probability_corpus
+    @tuples = []
+
+    read_probabilities!
+  end
+
+  def possible_next_letters(letter)
+    # Should return an array of possible next letters sorted
+    # by likelyhood in a descending order
+    letter_sets = tuples.select { |tuple| tuple.keys[0][0] == letter }
+    letter_sets.sort_by { |tuple| tuple.values[0] }.map { |tuple| tuple.keys[0][1] }
+  end
+
+  def most_common_next_letter(letter)
+    # The most probable next letter
+    possible_next_letters(letter).first
+  end
+
+  def common_next_letter(letter, sample_limit = 2)
+    # Randomly select a common letter within a range defined by
+    # the sample limit as the lower bounds of a substring
+    possible_next_letters(letter).first(sample_limit).sample
+  end
+
+  def build_password_from(letter, password_length = 10)
+    password = letter.dup # we don't want to mutate letter
+    (password_length - 1).times do |index|
+      password << common_next_letter(password[index])
+    end
+
+    password
+  end
+
+  def recursive_build_password_from(password, password_length = 10)    
+    return password if password_length == 1
+
+    recursive_build_password_from(password + common_next_letter(password[-1]), password_length - 1)
+  end
+
+  private
+
+  def read_probabilities!
+    CSV.open(probability_corpus, 'r', { headers: true }).each do |line|
+      letter_pair = line.fetch("letter pair")
+      count = line.fetch("count").to_i
+      tuples << { letter_pair => count }
+    end
+  end
+end
+
+class PronounceablePasswordWithAGiantHash
   attr_reader :letter_hash, :probability_corpus
 
   def initialize(probability_corpus)
@@ -24,7 +78,7 @@ class PronounceablePassword
 
   def recursive_build_password_from(password, password_length = 10)    
     return password if password_length == 1
-    
+
     recursive_build_password_from(password + common_next_letter(password[-1]), password_length - 1)
   end
 
