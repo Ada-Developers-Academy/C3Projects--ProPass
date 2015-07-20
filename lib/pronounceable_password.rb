ea3ee026ef0b139c950c88f391e7a4ac5db19c34
Carly Jugler <carjug@gmail.com>
2015-07-19 19:31:19 -0700

    Adds way clunky common_next_letter method. Passing all rspecs

--- lib/pronounceable_password.rb
+++ lib/pronounceable_password.rb
@@ -7,6 +7,7 @@ class PronounceablePassword
   def initialize(probability_corpus)
     # probability corpus is the file location of the CSV with the
     # pre-calculated letter probability pairs
+
     @probability_corpus = probability_corpus
   end
 
@@ -33,6 +34,8 @@ class PronounceablePassword
   end
 
   def most_common_next_letter(letter)
+    possible_next_letters(letter)
+
     most_common_letter_pair = @sorted_letters.find { |pairs| string = pairs[0]
        string[0,1] == letter }
 
@@ -43,5 +46,20 @@ class PronounceablePassword
     # Randomly select a common letter within a range defined by
     # the sample limit as the lower bounds of a substring
 
+    possible_next_letters(letter)
+
+    common_letters = []
+
+    common_letter1 = @sorted_letters.sample
+
+    common_letter1 = common_letter1.first[-1]
+
+    common_letter2 = @sorted_letters.sample
+
+    common_letter2 = common_letter2.first[-1]
+
+    common_letters.push(common_letter1, common_letter2)
+
+    return common_letters.sample
   end
 end
